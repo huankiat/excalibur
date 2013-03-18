1e62fd4b03e648e4267f2802ce4e4be6903f3346
Huankiat Seh <huankiat.seh@gmail.com>
2013-03-18 17:32:27 +0800

    start of project. initialize git repo.

---
+++ processclick_excel/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel
+using System.Reflection
+
+namespace processclick_excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+            Excel.Range oRng;
+
+            try {
+                //start Excel 
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                //new workbook
+                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+
+
+            }
+        }
+    }
+}
