f06b77c59098aaf1075c87db7273e928d05d5ad7
Amerson Lin <amerson@processclick.com>
2013-03-25 15:01:21 +0800

    Added ExcaliburTest project.

---
+++ ExcaliburTests/ChannelTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ExcaliburTests
+{
+    [TestFixture]
+    public class ChannelTest
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.AreEqual(25, 10);
+        }
+    }
+}
