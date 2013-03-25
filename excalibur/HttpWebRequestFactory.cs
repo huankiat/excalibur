c0ca8ef2a4b5ef902f3282f48e21cee2552c15e9
Amerson Lin <amerson@processclick.com>
2013-03-25 17:23:18 +0800

    Channel uses HttpWebRequestFactory for decoupling.

---
+++ excalibur/HttpWebRequestFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Excalibur
+{
+    public interface IWebRequestFactory
+    {
+        HttpWebRequest Create(string url);
+    }
+
+    public class HttpWebRequestFactory : IWebRequestFactory
+    {
+        public HttpWebRequest Create(string url)
+        {
+            return (HttpWebRequest)WebRequest.Create(url);
+        }
+    }
+
+}
