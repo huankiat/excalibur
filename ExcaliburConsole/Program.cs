e5317d2c31d84171e0672dbdbbd7ab484495af9a
Huankiat Seh <huankiat.seh@gmail.com>
2013-04-02 10:21:19 +0800

    add console for debugging purpose.

---
+++ ExcaliburConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using Excalibur.Models;
+
+namespace ExcaliburConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel ch = new Channel();
+            AuthToken at = new AuthToken();
+
+            string token = ch.getToken("huankiat@processclick.com","password");
+            Console.Write("Token from Website: " + token + "\n");
+
+            at.setToken(token);
+            at.createCookieInContainer();
+            string txt = at.readTokenFromCookie();
+            Console.Write("Cookie Token: " + txt + "\n");
+
+
+            Console.Read();
+        }
+    }
+}
